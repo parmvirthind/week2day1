709c50d7ee2bb7c018a487f35410c3f1dd7a293a
Parmvir <parmvirsthind@gmail.com>
2017-09-11 18:34:04 +0000

    Completed first step in exercise.

---
+++ printInChunks.js
@@ -0,0 +1,24 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.hmtl'
+  };
+
+  https.get(requestOptions, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log(data + '\n*********************');
+    });
+
+    response.on('end', function (){
+      console.log("End of stream");
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
