4f9e6f66d82ae2b41214d2f52a8beb102db190c7
Parmvir <parmvirsthind@gmail.com>
2017-09-11 19:00:25 +0000

    Inserted a callback function that prints HTML

---
+++ function4.js
@@ -0,0 +1,29 @@
+var https = require('https');
+
+function getHTML(options, callback) {
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+
+    var rawData = '';
+    response.on('data', function (data) {
+      rawData += data;
+    });
+
+    response.on('end', function () {
+      printHTML(rawData);
+    });
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.hmtl'
+};
+
+getHTML(requestOptions);
